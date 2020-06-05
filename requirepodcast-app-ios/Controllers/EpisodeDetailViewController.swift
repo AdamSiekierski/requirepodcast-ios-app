5dfd4c59d1dfa31b481cb14b8029e8abeba6608b
Adam Siekierski <adam.siekiera@outlook.com>
2020-06-05 23:12:12 +0200

    Basic project structure

---
+++ requirepodcast-app-ios/Controllers/EpisodeDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EpisodeDetailViewController.swift
+//  requirepodcast-app-ios
+//
+//  Created by Adam Siekierski on 05/06/2020.
+//  Copyright © 2020 AdamSiekierski. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ requirepodcast-app-ios/Controllers/EpisodeListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  requirepodcast-app-ios
+//
+//  Created by Adam Siekierski on 05/06/2020.
+//  Copyright © 2020 AdamSiekierski. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeListViewController: UITableViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "episodeListCell", for: indexPath)
+    cell.textLabel!.text = "dupa"
+    
+    return cell
+  }
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+}
+

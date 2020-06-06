9d3e378a455d846a824afd8a028ca3e7c8b083d8
Adam Siekierski <adam.siekiera@outlook.com>
2020-06-06 11:24:47 +0200

    RSS Parsing

--- requirepodcast-app-ios/Controllers/EpisodeListViewController.swift
+++ requirepodcast-app-ios/Controllers/EpisodeListViewController.swift
@@ -12,12 +12,15 @@ class EpisodeListViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    self.getData()
   }
+
+  let episodes = [String:String]()
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "episodeListCell", for: indexPath)
     cell.textLabel!.text = "dupa"
-    
+
     return cell
   }
   
@@ -27,6 +30,14 @@ class EpisodeListViewController: UITableViewController {
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return 1
+  }
+  
+  func getData() {
+    let url = URL(string: "https://anchor.fm/s/139df89c/podcast/rss")!
+    let episodeParser = PodcastRSSParser()
+    episodeParser.parse(url: url) { episodes in
+      print(episodes)
+    }
   }
 }
 
